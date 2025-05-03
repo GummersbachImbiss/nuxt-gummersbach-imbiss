732911121bbfcbe29c165c85c749241a9c65eff3
Jonathan Martz <support@habit-streak.app>
2025-05-03 21:29:33 +0200

    install vite tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -8,5 +10,10 @@ export default defineNuxtConfig({
     domain: 'gummersbach-imbiss.de',
     apiHost: 'https://tracking.jmse.cloud'
   },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/plausible']
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['@nuxtjs/plausible']
 })
